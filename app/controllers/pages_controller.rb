d9ec96e2678698757869b60cfa0be9d534b18813
Syed Sulthan <syed_search@yahoo.com>
2016-05-06 18:46:10 +0000

    Question 1 Changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,15 @@
 class PagesController < ApplicationController
+  
   def home
     # foo = Foobar.new "baz"
     # @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    name_entered = params[:name]
+    adj_entered = params[:adjective]
+    @text = %Q{ Your name #{name_entered} is so #{adj_entered}}
+    @text = "You are nothing!" if name_entered.blank? || adj_entered.blank?
   end
 
   def age
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'age', to: 'pages#age'
+  post 'stringify'  => 'pages#stringify'
+  
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
